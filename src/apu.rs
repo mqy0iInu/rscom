de539ad412ffb9d4ea3408a049b50f587d55662b
FYuto <kr84Tsroot@gmail.com>
2023-06-06 07:52:55 +0900

    マルチスレッド化

---
+++ src/apu.rs
@@ -0,0 +1,20 @@
+pub fn apu_reset()
+{
+    // TODO :APU Init
+}
+
+pub fn apu_main()
+{
+    println!("[DEBUG] : APU Main Loop");
+}
+
+// ====================================== TEST ======================================
+#[cfg(test)]
+mod apu_test {
+
+    #[test]
+    fn apu_test() {
+        // TODO : APU Test
+    }
+}
+// ==================================================================================
---
+++ src/ppu.rs
@@ -0,0 +1,20 @@
+pub fn ppu_reset()
+{
+    // TODO :PPU Init
+}
+
+pub fn ppu_main()
+{
+    println!("[DEBUG] : PPU Main Loop");
+}
+
+// ====================================== TEST ======================================
+#[cfg(test)]
+mod ppu_test {
+
+    #[test]
+    fn ppu_test() {
+        // TODO : PPU Test
+    }
+}
+// ==================================================================================
