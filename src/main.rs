0480be5960a3f42f6ec4403cc8197957b6dbadbf
mqy0iInu <kr84Tsroot@gmail.com>
2023-06-11 08:19:17 +0900

    乱数の生成を忘れてた

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ use ppu::{ppu_reset, ppu_main};
 use apu::{apu_reset, apu_main};
 use std::thread;
 use std::time::Duration;
-
+use rand::Rng;
 use sdl2::event::Event;
 use sdl2::EventPump;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
+
 #[macro_use]
 extern crate lazy_static;
 
@@ -39,6 +40,9 @@ fn read_screen_state(cpu_rp2a03: &mut Option<RP2A03<u8>>, frame: &mut [u8; 32 * 
     let mut frame_idx = 0;
     let mut update = false;
     if let Some(cpu) = cpu_rp2a03 {
+        let mut rng = rand::thread_rng();
+        cpu.nes_mem.mem_write(0xfe, rng.gen_range(1..16) as u8);
+
         for i in 0x0200..0x600 {
             let color_idx = cpu.nes_mem.mem_read(i as u16);
             let (b1, b2, b3) = color(color_idx).rgb();
@@ -86,10 +90,8 @@ fn app_init()
 
 fn main()
 {
-    let mut screen_state = [0 as u8; 32 * 3 * 32];
-    let mut rng = rand::thread_rng();
-
     // init sdl2
+    let mut screen_state = [0 as u8; 32 * 3 * 32];
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
@@ -106,31 +108,30 @@ fn main()
         .create_texture_target(PixelFormatEnum::RGB24, 32, 32).unwrap();
 // ==================================================================================
     // [H/W Reset & App Init]
-
     let mut cpu_handler = cpu_reset();
     ppu_reset();
     apu_reset();
     app_init();
-// ==================================================================================
-// [Thred Main Loop]
+    // ==================================================================================
+    // [Thred Main Loop]
     let _cpu_thread = thread::spawn(|| {
         loop {
             cpu_main();
-            // thread::sleep(Duration::from_millis(300));
+            thread::sleep(Duration::from_millis(100));
         }
     });
 
     let _ppu_thread = thread::spawn(|| {
         loop {
             ppu_main();
-            thread::sleep(Duration::from_millis(100));
+            thread::sleep(Duration::from_millis(500));
         }
     });
 
     let _apu_thread = thread::spawn(|| {
         loop {
             apu_main();
-            thread::sleep(Duration::from_millis(100));
+            thread::sleep(Duration::from_millis(500));
         }
     });
 
@@ -141,12 +142,10 @@ fn main()
 
         if read_screen_state(&mut cpu_handler, &mut screen_state) {
             texture.update(None, &screen_state, 32 * 3).unwrap();
-
             canvas.copy(&texture, None, None).unwrap();
-
             canvas.present();
         }
-        // thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(100));
     }
 // ==================================================================================
 }
